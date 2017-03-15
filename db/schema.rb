57816a52bff6f5d0dc66fd618fb868b4e046667f
Aleksey-novka <paschenko.aleks@gmail.com>
2017-03-15 10:13:36 +0300

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314190726) do
+ActiveRecord::Schema.define(version: 20170314190727) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "description"
